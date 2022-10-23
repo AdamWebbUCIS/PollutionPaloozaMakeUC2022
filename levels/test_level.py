64911e86ea47aee2767d8cbaa6ebc42f8d1066a9
grace smith <gsmith12301@gmail.com>
2022-10-22 22:43:13 -0400

    trash class defined

--- levels/test_level.py
+++ levels/test_level.py
@@ -5,6 +5,7 @@ import pygame
 from player import Player
 from level import Level
 from objects.shark import Shark
+from objects.trash import Trash
 import random
 
 class TestLevel(Level):
@@ -14,13 +15,33 @@ class TestLevel(Level):
         self.screen_pos = screen_pos
 
         self.shark1 = Shark(100, 100, 100, 100, self.screen, self.screen_pos)
+        
+        self.trash_list = []
+        
+        for _ in range(10):
+            x = random.randint(0,1000)
+            y = random.randint(0, 800)
+            piece_of_trash = Trash(
+                x,
+                y,
+                100,
+                100,
+                self.screen,
+                self.screen_pos
+            )
+
+            self.trash_list.append(piece_of_trash)
+        
+
     def draw_level(self):
         super().draw_level()
         background = pygame.image.load(os.path.join("assets", "backgrounds", "background.jpg")).convert_alpha()
         
         self.screen.blit(background, (0-self.screen_pos[0],0-self.screen_pos[1]))
         self.shark1.blit()
-
+        
+        for trash in self.trash_list:
+            trash.blit()
         if self.player.rect.colliderect(self.shark1.get_rect()):
             print("COLLIDED WITH SHARK1")
 
---
+++ objects/trash.py
@@ -0,0 +1,21 @@
+import os
+import pygame
+from objects.object import BaseObject
+from random import randint, choice
+
+class Trash(BaseObject):
+        def __init__(self, x, y, width, height, screen, screen_pos) -> None:
+            super().__init__(x, y, width, height, screen, screen_pos)
+            self.screen_pos = screen_pos
+
+            self.trash = {
+                "bottle": pygame.transform.scale(pygame.image.load(os.path.join("assets", "sprites", "Bottle 1.png")).convert_alpha(),(self.width, self.height)),
+                "bottle_two": pygame.transform.scale(pygame.image.load(os.path.join("assets", "sprites", "Bottle.png")).convert_alpha(),(self.width, self.height)),
+                "recycle":  pygame.transform.scale(pygame.image.load(os.path.join("assets", "sprites", "Recycle_bin.png")).convert_alpha(),(self.width, self.height)),
+                "tire": pygame.transform.scale(pygame.image.load(os.path.join("assets", "sprites", "Tire.png")).convert_alpha(),(self.width, self.height))
+            }
+
+            self.random_sprite = pygame.transform.rotozoom(choice(list(self.trash.values())), 0, 0.5)
+
+        def blit(self):
+            self.screen.blit(self.random_sprite, (self.x, self.y))
